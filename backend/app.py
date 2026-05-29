cdd2c555c2b39ee4bbf203c6c6edfc3eb66adcfb
Bea Clarice Hernandez <beaclaricemagsino18@gmail.com>
2026-05-29 23:41:57 +0800

    added host and env to backend

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -16,4 +17,5 @@ def health():
 
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
